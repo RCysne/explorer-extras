51ae73044a176809927d80544f77618b1e06b7e5
Ronaldo Cysne <ronaldocysne@gmail.com>
2024-02-16 15:51:10 -0300

    Atualizando repositório

--- gitfav/main.js
+++
@@ -1,8 +0,0 @@
-import { FavoritesView } from './js/Favorites.js'
-
-console.log(GitHubUser.search('rcysne'))
-
-new FavoritesView('#app')
-
-// PAPAI LINDÃO E FOFO E TANBEN BARRIGUDU
-// LENBRANSA  PRA  SENPRE  ASSINADU  LUISA  COM 6 ANOS
